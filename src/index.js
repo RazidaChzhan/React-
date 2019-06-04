20df299cfde5029ae9b4e6cfd46622e465cc039b
RazidaChzhan <chrazida@gmail.com>
2019-06-04 22:41:51 +0600

    Год выводится через props

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React, {Fragment} from "react";
 import ReactDOM from "react-dom";
-import './style.css'
 
-// class Counter extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       counter: 0,
-//       date: new Date(),
-//     }
-//   }
-// }
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      date: new Date(),
+      year: new Date(),
+    }
+  }
 
-  class DateView extends React.Component {
-   render () {
-    return (
-      <Fragment>
-        <h3>Year is {this.props.year.getFullYear()}.</h3>
-      </Fragment>
-    ) 
-   }
-   
   onClickUp = () => {
     this.setState((prevState) => {
       return {
@@ -28,27 +18,44 @@ import './style.css'
       }
     })
   };
-
+  
   onClickDate = () => {
-    this.setState((prevState) => {
-      return {
-        date: new Date (),
-      }
-    });
+        this.setState((prevState) => {
+          return {
+            date: new Date (),
+          }
+        });
+      };
+  
+  render() {
+      return (
+        <Fragment>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.onClickUp}>Click me!</button>
+        <h2>Time is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.onClickDate}>Refresh current time!</button>
+        <h3>Year is {this.state.year.getFullYear()}.</h3>
+        </Fragment>
+      );
+  }
+}
+
+class DataView extends React.Component {
+  constructor() {
+    super();
+    this.props = {
+      year: new Date(),
+    }
   }
 
-  // ReactDomrender() {
-  //     return (
-  //       <Fragment>
-  //         <h1>{this.state.counter}</h1>
-  //         <button onClick={this.onClickUp}>Click me!</button>
-  //         <h2>Today is {this.state.date.toLocaleDateString()}.</h2>
-  //         <button onClick={this.onClickDate}>Refresh current date!</button>
-          
-  //       </Fragment>
-  //     )
-  // }
+  render() {
+    return (
+      <Fragment>
+        <h3>Year is {this.props.year.getFullYear()}.</h3>
+      </Fragment>
+    );
+}
+}
 
-
-  const rootElement = document.getElementById("root");
-  ReactDOM.render(<Counter />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
