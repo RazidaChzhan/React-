e2c3294abc6beeeefcc69c249d8e741ed0ea6b0f
RazidaChzhan <chrazida@gmail.com>
2019-06-05 21:04:13 +0600

    Вызов DataView реализован

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React, {Fragment} from "react";
 import ReactDOM from "react-dom";
 
 class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0,
-      date: new Date(),
-      year: new Date(),
+
+  state = {
+    counter: 0,
+    date: new Date(),
+    year: new Date(),
     }
-  }
 
   onClickUp = () => {
     this.setState((prevState) => {
@@ -22,7 +20,7 @@ class Counter extends React.Component {
   onClickDate = () => {
         this.setState((prevState) => {
           return {
-            date: new Date (),
+            date: new Date (), 
           }
         });
       };
@@ -30,32 +28,37 @@ class Counter extends React.Component {
   render() {
       return (
         <Fragment>
-        <h1>{this.state.counter}</h1>
-        <button onClick={this.onClickUp}>Click me!</button>
-        <h2>Time is {this.state.date.toLocaleTimeString()}.</h2>
-        <button onClick={this.onClickDate}>Refresh current time!</button>
-        <h3>Year is {this.state.year.getFullYear()}.</h3>
+          <button onClick = {() => {this.props.updateData(this.state.year)}}>Обновить год</button>
+          <h1>{this.state.counter}</h1>
+          <button onClick={this.onClickUp}>Click me!</button>
+          <h2>Time is {this.state.date.toLocaleTimeString()}.</h2>
+          <button onClick={this.onClickDate}>Refresh current time!</button>
         </Fragment>
       );
   }
 }
 
 class DataView extends React.Component {
-  constructor() {
-    super();
-    this.props = {
-      year: new Date(),
-    }
+  state = {
+    year: ''
   }
+
+  updateData = (value) => {
+    this.setState({ year: value.getFullYear() })
+  }
+
 
   render() {
     return (
       <Fragment>
-        <h3>Year is {this.props.year.getFullYear()}.</h3>
+        <h3>Year is {this.state.year}.</h3>
+        <Counter updateData={this.updateData}/>
       </Fragment>
     );
-}
+  }
 }
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(<Counter />, rootElement);
+ReactDOM.render(<DataView />, document.getElementById('root'));
